6d3e9348374f8649fa941737ac946e64d68bab29
abdullahbasir <abdullah.sbasir@gmail.com>
2026-09-14 20:02:29 +0400

    Initialized the server with espress

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello, World!")
+import express, { type Express } from 'express';
+
+const app: Express = express();
+const port = 8080;
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
