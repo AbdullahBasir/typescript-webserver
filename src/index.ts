b2f1165a26b7e5107384576b1b8d6935fc13b1f6
abdullahbasir <abdullah.sbasir@gmail.com>
2026-09-15 13:58:38 +0400

    Added a readiness handler

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
-import express, { type Express } from 'express';
+import express, { type Express, type Request, type Response } from 'express';
 
 const app: Express = express();
 const port = 8080;
 
+app.use("/app", express.static("./src/app"))
+app.use("/assets", express.static("./src/app/assets"))
+
+app.get('/healthz', (req: Request, res: Response) => {
+    res.status(200)
+    res.set('Content-Type', 'text/plain; charset=utf-8')
+    res.send("OK");
+});
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
