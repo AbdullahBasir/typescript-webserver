1c81e4712136599473c8bc20884cc5086ad6fc27
abdullahbasir <abdullah.sbasir@gmail.com>
2026-09-15 15:32:02 +0400

    Added a logging middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-import express, { type Express, type Request, type Response } from 'express';
+import express, { type Express, type Request, type Response, type NextFunction } from 'express';
 
 const app: Express = express();
 const port = 8080;
 
+const middlewareLogResponses = (req: Request, res: Response, next: NextFunction) => {
+    res.on("finish", () => {
+        const status = res.statusCode;
+        if (status === 200) {
+            console.log(`[OK] ${req.method} ${req.url} - Status: ${status}`);
+        }
+        console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${status}`);
+    })
+    next()
+};
+
+app.use(middlewareLogResponses);
 app.use("/app", express.static("./src/app"))
 app.use("/assets", express.static("./src/app/assets"))
 
