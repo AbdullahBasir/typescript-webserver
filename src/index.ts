3800a499ec421580c0439470d24e12ce361fdb29
abdullahbasir <abdullah.sbasir@gmail.com>
2026-09-15 18:47:06 +0400

    Added a request count middleware

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { type Express, type Request, type Response, type NextFunction } from 'express';
+import { config } from './config.js';
 
 const app: Express = express();
 const port = 8080;
@@ -14,14 +15,28 @@ const middlewareLogResponses = (req: Request, res: Response, next: NextFunction)
     next()
 };
 
+const middlewareMetricsInc = (req: Request, res: Response, next: NextFunction) => {
+    config.fileserverHits++;
+    next()
+};
+
 app.use(middlewareLogResponses);
-app.use("/app", express.static("./src/app"))
+app.use("/app", middlewareMetricsInc, express.static("./src/app"))
 app.use("/assets", express.static("./src/app/assets"))
 
 app.get('/healthz', (req: Request, res: Response) => {
     res.status(200)
     res.set('Content-Type', 'text/plain; charset=utf-8')
     res.send("OK");
+});
+
+app.get('/metrics', (req: Request, res: Response) => {
+    res.send(`Hits: ${config.fileserverHits}`);
+});
+
+app.get('/reset', (req: Request, res: Response) => {
+    config.fileserverHits = 0;
+    res.send(`Hits: ${config.fileserverHits}`);
 });
 
 app.listen(port, () => {
