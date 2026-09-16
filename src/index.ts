d568ce81335d6cd73821f286cb9265144692ddf4
abdullahbasir <abdullah.sbasir@gmail.com>
2026-09-16 18:08:02 +0400

    Added a message length validation handler

--- src/index.ts
+++ src/index.ts
@@ -51,6 +51,35 @@ app.get('/admin/reset', (req: Request, res: Response) => {
     res.send(`Hits: ${config.fileserverHits}`);
 });
 
+app.post('/api/validate_chirp', (req: Request, res: Response) => {
+    let body = "";
+
+    req.on("data", (chunk) => {
+        body += chunk;
+    });
+
+    req.on("end", () => {
+        try {
+            const parsedBody = JSON.parse(body);
+            if (parsedBody.body.length >= 140) {
+                res.status(400).format({
+                    'appliation/json'() {
+                        res.send({error: 'Chirp is too long'});
+                    }
+                });
+            } else {
+                res.status(200).format({
+                    'appliation/json'() {
+                        res.send({valid: true});
+                    }
+                });
+            }
+        } catch (error) {
+            res.status(400).send("Invalid JSON");
+        }
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
