e908c99088203b6630a55c728ec1f1024a07e77d
abdullahbasir <abdullah.sbasir@gmail.com>
2026-09-20 06:44:17 +0400

    Added postgres database and drizzle migration tools

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,19 @@
 type APIConfig = {
   fileserverHits: number;
+  dbUrl: string
 };
 
+process.loadEnvFile();
+
+const envOrThrow = (key: string) => {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`environment variable ${key} not set`);
+  }
+  return value;
+}
+
 export const config: APIConfig = {
-    fileserverHits: 0
+    fileserverHits: 0,
+    dbUrl: envOrThrow("DB_URL"),
 };
