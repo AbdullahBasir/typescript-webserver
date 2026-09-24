bb0b59dcea8e8d216e955b89c18893c9a342ed49
abdullahbasir <abdullah.sbasir@gmail.com>
2026-09-24 13:16:55 +0400

    Added a query to filter chirps based on id

--- src/api/get_chirps.ts
+++ src/api/get_chirps.ts
@@ -1,10 +1,22 @@
 import { Request, Response } from 'express';
-import { getChirps } from '../db/queries/chirps.js';
+import { getChirps, getChirpsById } from '../db/queries/chirps.js';
 import { BadRequest } from '../errors.js';
 import { RespondWithJSON } from './json.js';
 
 
 export const getChirpsHandler = async (req: Request, res: Response) => {
+    let authorId = "";
+    let authorIdQuery = req.query.authorId;
+    if (typeof authorIdQuery === "string") {
+        authorId = authorIdQuery;
+    }
+
+    const chirpsByAuthor = await getChirpsById(authorId)
+    if (chirpsByAuthor) {
+        RespondWithJSON(res, 200, chirpsByAuthor);
+        return;
+    }
+
     const chirps = await getChirps();
     if (!chirps || chirps.length === 0) {
         throw new BadRequest("could not get chirps");
--- src/db/queries/chirps.ts
+++ src/db/queries/chirps.ts
@@ -1,6 +1,6 @@
 import { asc, eq, and } from 'drizzle-orm';
 import { db } from "../index.js";
-import { NewChirp, chirps, users } from "../schema.js";
+import { NewChirp, chirps } from "../schema.js";
 
 export async function createChirp(chirp: NewChirp) {
     const [result] = await db
@@ -14,6 +14,15 @@ export async function getChirps() {
     const result = await db
     .select()
     .from(chirps)
+    .orderBy(asc(chirps.createdAt))
+    return result;
+}
+
+export async function getChirpsById(authorId: string) {
+    const result = await db
+    .select()
+    .from(chirps)
+    .where(eq(chirps.userId, authorId))
     .orderBy(asc(chirps.createdAt))
     return result;
 }
